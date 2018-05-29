e971080b4f1814c342b031229e2eda5a60c0a45c
ioana <geantaioana13@stud.ase.ro>
2018-05-29 15:42:06 +0300

    check with agent

--- src/main/java/com/webstudent/platform/controller/SubjectController.java
+++ src/main/java/com/webstudent/platform/controller/SubjectController.java
@@ -4,6 +4,7 @@ import com.webstudent.platform.repository.SubjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +25,63 @@ public class SubjectController {
     @GetMapping("/getAbsences")
     public Integer getAbsences() {
         return subjectRepository.countAbsences();
+    }
+
+    @PostMapping("/fulfill")
+    public String postFulfillement() {
+        return "{\n" +
+                "\"fulfillmentText\": \"HELLLLOOO\",\n" +
+                "\"fulfillmentMessages\": [\n" +
+                "  {\n" +
+                "    \"card\": {\n" +
+                "      \"title\": \"card title\",\n" +
+                "      \"subtitle\": \"card text\",\n" +
+                "      \"imageUri\": \"https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png\",\n" +
+                "      \"buttons\": [\n" +
+                "        {\n" +
+                "          \"text\": \"button text\",\n" +
+                "          \"postback\": \"https://assistant.google.com/\"\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  }\n" +
+                "],\n" +
+                "\"source\": \"example.com\",\n" +
+                "\"payload\": {\n" +
+                "  \"google\": {\n" +
+                "    \"expectUserResponse\": true,\n" +
+                "    \"richResponse\": {\n" +
+                "      \"items\": [\n" +
+                "        {\n" +
+                "          \"simpleResponse\": {\n" +
+                "            \"textToSpeech\": \"HELLLLOOO this is a simple response\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"facebook\": {\n" +
+                "    \"text\": \"Hello, Facebook!\"\n" +
+                "  },\n" +
+                "  \"slack\": {\n" +
+                "    \"text\": \"This is a text response for Slack.\"\n" +
+                "  }\n" +
+                "},\n" +
+                "\"outputContexts\": [\n" +
+                "  {\n" +
+                "    \"name\": \"projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name\",\n" +
+                "    \"lifespanCount\": 5,\n" +
+                "    \"parameters\": {\n" +
+                "      \"param\": \"param value\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "],\n" +
+                "\"followupEventInput\": {\n" +
+                "  \"name\": \"event name\",\n" +
+                "  \"languageCode\": \"en-US\",\n" +
+                "  \"parameters\": {\n" +
+                "    \"param\": \"param value\"\n" +
+                "  }\n" +
+                "}";
     }
 }
